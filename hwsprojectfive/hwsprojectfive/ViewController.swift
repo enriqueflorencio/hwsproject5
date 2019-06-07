e65f62ce30f65bb5d1b72b4bd632550575e00377
enriqueflorencio <enrique.florencio@yahoo.com>
2019-06-07 02:46:56 -0700

    Finished implementing the submit button but still need to verify if the user's answers are correct. Might continue in the morning. I'm pooped lol

--- hwsprojectfive/hwsprojectfive/ViewController.swift
+++ hwsprojectfive/hwsprojectfive/ViewController.swift
@@ -43,6 +43,36 @@ class ViewController: UITableViewController {
         tableView.reloadData()
     }
     
+    func isPossible(word: String) -> Bool {
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        return true
+    }
+    
+    func submit(_ answer: String) {
+        /*We need to lower case every answer just in case the player tries to pull a fast one ex
+         Cease and cease are different. We need to lower case each one so that the user doesn't get
+         extra points */
+        let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usersWords.insert(answer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                }
+            }
+        }
+    }
+    
     //Define number of rows per usual
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return usersWords.count
@@ -60,15 +90,20 @@ class ViewController: UITableViewController {
         let ac = UIAlertController(title: "Enter Answer", message: nil, preferredStyle: .alert)
         ac.addTextField()
         
+        //Trailing closures aren't bad at all. Note: Refer to page 256 in case I find myself stuck again with the syntax
         let submitAction = UIAlertAction(title: "Submit", style: .default) {
-            [weak self, weak ac] action in
+            //action can be replaced with _ since we aren't using action at all in the closure
+            [weak self, weak ac] _ in
+            
+            //This line unwraps the array of text fields (the user's answer) hence, answer
             guard let answer = ac?.textFields?[0].text else {
                 return
             }
             self?.submit(answer)
-            ac?.addAction(submitAction)
-            present(ac, animated: true)
         }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
         
     }
     
