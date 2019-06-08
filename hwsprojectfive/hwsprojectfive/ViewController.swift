c2f5f450b5f952f4ce7a8ee95d43af0ff8edbe23
enriqueflorencio <enrique.florencio@yahoo.com>
2019-06-08 03:22:05 -0700

    completed anagram game without challenges done. Will get to them in a sec

--- hwsprojectfive/hwsprojectfive/ViewController.swift
+++ hwsprojectfive/hwsprojectfive/ViewController.swift
@@ -44,15 +44,34 @@ class ViewController: UITableViewController {
     }
     
     func isPossible(word: String) -> Bool {
+        
+        guard var tempWord = title?.lowercased() else {
+            return false
+        }
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        
+        
         return true
     }
     
     func isOriginal(word: String) -> Bool {
-        return true
+        return !usersWords.contains(word)
     }
     
     func isReal(word: String) -> Bool {
-        return true
+        
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let mispelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return mispelledRange.location == NSNotFound
     }
     
     func submit(_ answer: String) {
@@ -60,17 +79,36 @@ class ViewController: UITableViewController {
          Cease and cease are different. We need to lower case each one so that the user doesn't get
          extra points */
         let lowerAnswer = answer.lowercased()
+        let errorTitle: String
+        let errorMessage: String
         
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
+                    //Insert this word into the very beginning of the array
                     usersWords.insert(answer, at: 0)
                     
+                    //update the tableview to present the new word
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
+                    return
+                } else {
+                    errorTitle = "Word not recognised"
+                    errorMessage = "You can't just make them up, you know!"
                 }
+            } else {
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
             }
+        } else {
+            guard let title = title?.lowercased() else { return }
+            errorTitle = "Word not possible!"
+            errorMessage = "You can't spell that word from \(title)"
         }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OIK", style: .default))
+        present(ac, animated: true)
     }
     
     //Define number of rows per usual
